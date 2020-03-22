ce5deae0da59a07a5dfeb07803f036f93e8d6943
Jonathan-Lara-Hurtado <jonabimael@gmail.com>
2020-03-21 18:58:39 -0600

    "Funcion informacion tablas"

--- potrotransporte/static/potrotransporte/Herramientas.js
+++ potrotransporte/static/potrotransporte/Herramientas.js
@@ -1,3 +1,36 @@
-function informacionTablas(){
-console.debug("hola");
+function informacionTablas(nombre){
+   $(nombre).DataTable(
+
+   {
+        "language": {
+         "decimal":        "",
+    "emptyTable":     "Sin Articulos",
+    "info":           "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+    "infoEmpty":      "Mostrando 0 a 0 de 0 Entradas",
+    "infoFiltered":   "(filtrado de _MAX_ entradas totales)",
+    "infoPostFix":    "",
+    "thousands":      ",",
+    "lengthMenu":     "Mostrar _MENU_ entradas",
+    "loadingRecords": "Cargando...",
+    "processing":     "Procesando...",
+    "search":         "Buscar:",
+    "zeroRecords":    "No matching records found",
+    "paginate": {
+        "first":      "Primero",
+        "last":       "Anterior",
+        "next":       "Siguiente",
+        "previous":   "Previo"
+    },
+    "aria": {
+        "sortAscending":  ": activate to sort column ascending",
+        "sortDescending": ": activate to sort column descending"
+    }
+
+
+
+        }
+    }
+
+    );
+
 }
